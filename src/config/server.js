7535169e0c5fc7b10c52558dd74f8ed530dcb90f
Caio Rodrigues <rfcaio90@gmail.com>
2019-04-05 13:33:10 -0300

    node1-aula-05

--- src/config/server.js
+++ src/config/server.js
@@ -2,8 +2,12 @@ require('marko/node-require').install()
 require('marko/express')
 
 const bodyParser = require('body-parser')
+const express = require('express')
 
-const app = require('express')()
+const app = express()
+
+app.use('/static', express.static('src/app/public'))
+
 app.use(bodyParser.urlencoded({
   extended: true
 }))
